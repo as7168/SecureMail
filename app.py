a58d0ebcf00e744f07dddccbd9abb1694e35966b
as7168 <amn@Amn.localdomain>
2018-04-21 17:45:33 -0500

    webui hello world app runs on localhost

--- app.py
+++ app.py
@@ -1,2 +1,13 @@
 #!usr/bin/env python
+from flask import Flask
 
+app = Flask(__name__)
+
+@app.route('/')
+def route():
+	return 'Hello World'
+
+
+
+if __name__ == '__main__':
+	app.run(debug=True)
