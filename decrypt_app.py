e5c3ca4eda486b326f3eb9245dcfe51ee0f10725
as7168 <amn@Amn.localdomain>
2018-04-27 17:40:24 -0500

    decryption initial commit

---
+++ decrypt_app.py
@@ -0,0 +1,111 @@
+#!usr/bin/env python
+from random import randint
+from flask import Flask, request, render_template, url_for, redirect, session
+import smtplib
+from email.mime.text import MIMEText
+
+import time
+from itertools import chain
+import email
+import imaplib
+
+app = Flask(__name__, template_folder='templates')
+
+server = None
+
+@app.route('/login', methods=['GET', 'POST'])
+@app.route('/', methods=['GET', 'POST'])
+def login():
+	if request.method == 'POST':
+		global server
+		#print 'inpost'
+		user = request.form['uname'].strip()
+		password = request.form['pswd'].strip()
+		mail_server, port = get_mail_server_and_port(user.split('@')[1])
+		if mail_server is None:
+			return redirect(url_for('login'))
+		try:
+			server.quit()
+		except:
+			pass
+		server = imaplib.IMAP4_SSL(mail_server, port)
+		try:
+			server.login(user, password)
+			server.select('INBOX')
+		except:
+			return 'Authentication Error! Try Logging in again'
+		session['username'] = user
+		return redirect(url_for('index', user=user))
+	else:
+		return render_template('login.html')
+
+
+@app.route('/index', methods=['GET', 'POST'])
+def index():
+	if request.method == 'POST':
+		global server
+		#print 'in post'
+		if 'username' in session:
+			from_ = request.form['from'].strip()
+			subj = request.form['subject'].strip()
+			query = '(FROM "'+from_+'" SUBJECT "'+subj+'")'
+			result, data = server.uid('search', None, query)
+			print result
+			print data
+			if result == 'OK':
+				return redirect(url_for('view', data=data[0]))
+			else:
+				return render_template('decrypt_index.html', user=request.args['user'])
+			#print send_to, key, subj, body
+		else:
+			return redirect(url_for('login'))
+	
+	#print request.args['user']
+	if request.args['user'] is not None:
+		if 'username' in session:
+			if session['username'] == request.args['user']:
+				return render_template('decrypt_index.html', user=request.args['user'])
+			else:
+				session.pop('username')
+
+	return redirect(url_for('login'))
+
+@app.route('/view', methods=['GET'])
+def view():
+	#decryt data here
+	global server
+	print 'in view'
+	uids = request.args['data']
+	print 'before fetching'
+	result, data = server.uid('fetch', int(uids.split()[0]), '(RFC822)')
+	if result == 'OK':
+		#print data
+		return data[0]
+
+@app.route('/logout', methods=['GET'])
+def logout():
+	global server
+	if len(session) != 0:
+		user1 = ''
+		password1 = ''
+		session.pop('username')
+		try:
+			server.logout()
+		except:
+			pass
+	#return 'You have logged out'
+	return redirect(url_for('login'))
+
+
+def get_mail_server_and_port(mail_server):
+	if mail_server == 'yahoo.com':
+		return 'imap.mail.yahoo.com', 993
+	if mail_server == 'gmail.com':
+		return 'imap.gmail.com', 993
+	else:
+		return None, None
+
+if __name__ == '__main__':
+	app.secret_key = str(randint(1, 1000000))
+	app.debug = True
+	app.run()
