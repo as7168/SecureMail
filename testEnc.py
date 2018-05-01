d3f477a544742a4208770aa14c2dde82b8dda6e6
as7168 <amn@Amn.localdomain>
2018-05-01 17:48:44 -0500

    git issues

--- testEnc.py
+++ testEnc.py
@@ -1,6 +1,11 @@
-from SecureEmail import SecureEmail
+from SecureEmail import SecureEmail_func
 
 message = 'asjdabsjhdbvajsbj'
+msb = message.encode('utf-8')
 
-enc, cip = SecureEmail(1, message)
+enc, cip = SecureEmail_func(1, False, msb)
+print(type(cip))
 print (cip)
+dec = SecureEmail_func(2, True, cip)
+
+print (str(dec))
